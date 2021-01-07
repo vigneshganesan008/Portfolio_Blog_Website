c48a16ce6b5d452f2bf41faf17a3ad1ed02db295
Vignesh Ganesan <vicky08rockz@gmail.com>
2021-01-07 11:17:02 +0530

    dynamic url for blogs

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path("",views.allBlogs)
+    path("",views.allBlogs, name = "allBlogs"),
+    path("<int:blog_id>/",views.blogDetails, name = "blogDetails")
 ] 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allBlogs(request):
     blogs = Blog.objects.order_by("-date")[:5]
     return render(request, "blog/allBlogs.html",{"blogs":blogs})
+
+def blogDetails(request, blog_id):
+    blog = get_object_or_404(Blog,pk = blog_id)
+    return render(request, "blog/blogDetails.html",{"blog": blog})

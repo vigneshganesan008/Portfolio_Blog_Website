ad8ad307fee0f000ca331b811983c784b8313c50
Vignesh Ganesan <vicky08rockz@gmail.com>
2021-01-08 13:36:11 +0530

    functionality compeleted

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     discription = TextField()
     date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
--- portfolio/models.py
+++ portfolio/models.py
@@ -7,3 +7,6 @@ class Project (models.Model):
     discription = models.CharField(max_length=100)
     image =  models.ImageField(upload_to = "portfolio/images")
     url = models.URLField(blank=True)
+
+    def __str__(self) :
+        return self.title

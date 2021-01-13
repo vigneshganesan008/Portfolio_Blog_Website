8c884b3a7a6f9426b7052c6bc8432b79276c484a
Vignesh Ganesan <vicky08rockz@gmail.com>
2021-01-13 18:34:01 +0530

    Bootstrapped Home page

--- Portfolio_Blog/urls.py
+++ Portfolio_Blog/urls.py
@@ -21,6 +21,6 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.home),
+    path("",views.home,name="home"),
     path('blog/', include('blog.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
